fdf6811f987737c2644c0b8c3dc30f9e021a3d91
Jym Bocala <jym.bocala@gmail.com>
2022-10-28 21:44:16 +1100

    update image alts

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,19 @@
-import React from "react"
+import React from "react";
 
 export default function Card() {
-    return (
-        <div className="card">
-            <img src="../images/katie-zaferes.png" className="card--image" />
-            <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
-                <span>5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
-            </div>
-            <p>Life Lessons with Katie Zaferes</p>
-            <p><span className="bold">From $136</span> / person</p>
-        </div>
-    )
+  return (
+    <div className="card">
+      <img src="../images/katie-zaferes.png" className="card--image" alt="Katie Zaferes" />
+      <div className="card--stats">
+        <img src="../images/star.png" className="card--star" alt="star" />
+        <span>5.0</span>
+        <span className="gray">(6) • </span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life Lessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From $136</span> / person
+      </p>
+    </div>
+  );
 }
