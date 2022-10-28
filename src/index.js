37be1edec441576ca7985a0e729f3f0f1950754a
Jym Bocala <jym.bocala@gmail.com>
2022-10-28 18:10:45 +1100

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode> 
+    <App />
+  </React.StrictMode>
+);
+
+
+// StrictMode is a tool for highlighting potential problems in an application. Like Fragment, StrictMode does not render any visible UI. It activates additional checks and warnings for its descendants.
